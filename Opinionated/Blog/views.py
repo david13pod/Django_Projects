93e967e92e9582776561370784bfe11d8ccc86d5
Oluwapelumi <odedereoluwapelumi13@gmail.com>
2021-03-17 14:01:58 +0100

    views and urrls

--- Opinionated/Blog/views.py
+++ Opinionated/Blog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-from django.views.generic import (TemplateView,ListView, DetailView,DeleteView)
+from django.shortcuts import render,get_object_or_404,redirect
+from django.views.generic import (TemplateView,ListView, DetailView,DeleteView, CreateView,UpdateView)
 from Blog.models import Post, Comment
+from Blog.forms import PostForm, CommentForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
@@ -11,8 +16,67 @@ class PostListView (ListView):
     model=Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
 
 class PostDetailView (DeleteView):
     model=Post
+
+
+class PostCreateView (LoginRequiredMixin, CreateView):
+    login_url='/login/'
+    form_class= PostForm
+    redirect_field_name='blog/post_detail.html'
+    model=Post
+
+class PostUpdateView (LoginRequiredMixin, UpdateView):
+    login_url='/login/'
+    form_class= PostForm
+    redirect_field_name='blog/post_detail.html'
+    model=Post
+
+class PostDeleteView (LoginRequiredMixin, DeleteView):
+    model=Post
+    success_url=reverse_lazy('post_list')
+
+class DraftListView (LoginRequiredMixin, ListView):
+    login_url='/login/'
+    model=Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('create_date')
+
+
+
+@login_required
+def add_comment_to_post(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    if request.method =='POST':
+        form=CommentForm(request.POST)
+        if form.is_valid():
+            comment=form.save(commit=False)
+            comment.post=post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form=CommentForm()
+    return render(request,'Blog/comment_form.html',{'form':form})
+
+@login_required
+def comment_approve(request,pk):
+    comment=get_object_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect('post_details',pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+    comment=get_object_or_404(Comment,pk=pk)
+    post_pk= comment.post.pk
+    comment.delete()
+    return redirect('post_details',pk=post_pk)
+
+@login_required
+def post_publish(request,pk):
+    post= get_object_or_404(Post,pk=pk)
+    post.publish
+    return redirect('post_details',pk=pk)
